de9dc377edd4ed0ab44002eb31055a2d2545fc4c
dev-Aarish <aritrobag362@gmail.com>
2026-03-07 20:13:55 +0530

    first commit

---
+++ contracts/FundMe.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+/*
+Basic Idea of the contract:
+1.Get funds from the users
+2.Withdraw those deposited funds
+3.Set a minimum funding value in USD
+*/
+
