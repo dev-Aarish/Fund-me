056391306d8f0d60227e4893fc74ea9b2d84c173
dev-Aarish <aritrobag362@gmail.com>
2026-03-08 09:00:54 +0530

    getPrice() and interface added

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -8,15 +8,36 @@ Basic Idea of the contract:
 3.Set a minimum funding value in USD
 */
 
+//This is the interface used for getting the ABI. We are importing the interface from the gitHub.
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
 contract fundMe{
-    function fund() public payable{
+
+    uint256 public minimumUSD=5;
+
+    function fund() public payable{ //to recieve native blockchain tokens by a function, we need to add the 'payable' keyword
         //Allow user to send $
         //Have a min amt to be funded
         //How do we send ETH to this contract?
-        require(msg.value>1 ether,"Didn't send enough ETH");   //1e18 wei=1ETH, This value is the field present in the deploy section in remix IDE.
+        require(msg.value>= minimumUSD,"Didn't send enough ETH");   //1e18 wei=1ETH, This value is the field present in the deploy section in remix IDE.
     }
 
     // function withdraw() public{}
+
+    function getPrice() public view returns(uint256){ //to get the current price of ETH in terms of USD.
+        //Address: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        //ABI
+        AggregatorV3Interface priceFeed=AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+        //ETH/USD rate in 18 digit
+        return uint256(price*1e10);
+    }    
+
+    function getConversionRate() public{}   //converts a value to its converted value based off of its price.
+
+    function getVersion() public view returns(uint256){
+        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+    }
 
 }
 
@@ -25,4 +46,11 @@ What I got to learn:
 1. Sending ETH through a function
     The "value" in the 'deploy and run txn' can be used to mention the amt to tokens(ETH) that can be sent to some other users.
     The keyword "payable" is used for token handling for a function. This also make the button red in the after deployment of the contract.
+2. What is a revert?
+    Undo any action that has been done, and send the remaining gas back. 
+    Which basically means, that all the modifications that has been done(state modification) during the running of the function will be changed back to its normal state.
+3. Chainlink : Decentralized Oracle Network
+    The USD price of assets like Ethereum cannot be derived from blockchain technolofy alone but it is determined by the financial markets.
+    To obtain a correct price info, a connection b/w off-chain and on-chain data is necessary. This is facilitated by a decentralized Oracle n/w.  
+4. Interface        
 */
