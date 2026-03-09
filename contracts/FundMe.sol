0b7316de739157999af055e4b2e4ff0a79031e29
dev-Aarish <aritrobag362@gmail.com>
2026-03-09 07:04:31 +0530

    gas optimization

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -13,25 +13,27 @@ Basic Idea of the contract:
 
 import {PriceConverter} from "./PriceConverter.sol";
 
+error NotOwner();
+
 contract fundMe{
 
     using PriceConverter for uint256;
 
-    uint256 public minimumUSD=5e18;//This is the hardcoded amount of 5$ amt at-least need to be funded.
+    uint256 public constant MINIMUM_USD=5e18;//This is the hardcoded amount of 5$ amt at-least need to be funded.
     address[] public funders;
     mapping(address funder =>uint256 amountFunded) public addressToAmountFunded;
 
-    address public owner;
+    address public immutable i_owner;
 
     constructor(){
-        owner=msg.sender;
+        i_owner=msg.sender;
     }
 
     function fund() public payable{ //to recieve native blockchain tokens by a function, we need to add the 'payable' keyword
         //Allow user to send $
         //Have a min amt to be funded
         //How do we send ETH to this contract?
-        require(msg.value.getConversionRate()>= minimumUSD,"Didn't send enough ETH");   //1e18 wei=1ETH, This value is the field present in the deploy section in remix IDE.
+        require(msg.value.getConversionRate()>= MINIMUM_USD,"Didn't send enough ETH");   //1e18 wei=1ETH, This value is the field present in the deploy section in remix IDE.
         //In the above line of code, we imported the function getConversionRate from the PriceConverter lib and point to be noted, that even though getConversionRate function takes one uint256, nothing id passes as an arg.
         //That is becasue the msg.value itself gets passed as the parameter in the function while calling.
         funders.push(msg.sender);
@@ -58,7 +60,10 @@ contract fundMe{
     }
 
     modifier onlyOwner(){
-        require(msg.sender==owner,"Must be the owner!");
+        // require(msg.sender==i_owner,"Must be the owner!");
+        if(msg.sender!=i_owner){
+            revert NotOwner();
+        }
         _;
     }
 
